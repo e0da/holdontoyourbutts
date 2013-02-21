9dba75b7847d9955be1562eb1c78a3f3ec593ea7
Justin Force <justin.force@gmail.com>
2013-02-20 16:47:47 -0800

    Center; maintain aspect ratio; remove scroll bars
    1. Center vertically and horizontally
    2. Maintain aspect ratio
    3. Remove scroll bars
    4. BONUS! Now shakes more violently!

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
 (function () {
 
-  var sam;
+  var sam, div, top;
 
-  /**
-   * Return a random integer between -1 and 1, inclusive.
-   */
   function randomOffset() {
-    return Math.floor(Math.random() * 15) - 1 + 'px';
+    var range = 15;
+    return Math.floor(Math.random() * range) - Math.ceil(range / 2);
   }
 
   function wiggle() {
+    setDimensions();
+    setPosition();
+  }
+
+  function setDimensions() {
+    var scaleWidth, scaleHeight, scaleRatio, newWidth, newHeight;
+    scaleWidth  = top.width()  / sam.width();
+    scaleHeight = top.height() / sam.height();
+    scaleRatio  = Math.min(scaleWidth, scaleHeight);
+    newWidth  = sam.width()  * scaleRatio;
+    newHeight = sam.height() * scaleRatio;
     sam.css({
-      marginTop:  randomOffset(),
-      marginLeft: randomOffset()
+      width:  newWidth,
+      height: newHeight
     });
   }
 
-  $(function () {
+  function setPosition() {
+    var baseLeft, baseTop, newTop, newLeft;
+    baseLeft = top.width() / 2  - sam.width()  / 2;
+    baseTop  = top.height() / 2 - sam.height() / 2;
+    newTop  = baseTop  + randomOffset() + 'px';
+    newLeft = baseLeft + randomOffset() + 'px';
+    sam.css({
+      marginTop:  newTop,
+      marginLeft: newLeft
+    });
+  }
+
+  function cacheQueries() {
     sam = $('img');
+    div = $('div');
+    top = $(window.top);
+  }
+
+  $(function () {
+    cacheQueries();
     setInterval(wiggle, 50);
   });
 }(this));
