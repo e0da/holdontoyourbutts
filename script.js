8299324a690a2732ab037ad947e2ad466a0be346
Roberto Aguilar <roberto@baremetal.io>
2013-02-26 10:15:57 -0800

    Full screen sam action!

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
    * Return a random integer between -1 and 1, inclusive.
    */
   function randomOffset() {
-    return Math.floor(Math.random() * 3) - 1 + 'px';
+    return Math.floor(Math.random() * 15) - 1 + 'px';
   }
 
   function wiggle() {
